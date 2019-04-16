f1c4714d902529153f0f5e15c656fb3979de364c
Artur Pietrzyk <artudi54@gmail.com>
2019-04-16 22:41:21 +0200

    Fixed bug in update commands

--- lib/command/update.sh
+++ lib/command/update.sh
@@ -3,7 +3,7 @@ DIRECTORY="$(dirname "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")")"
 source "$DIRECTORY/util/configure.sh"
 source "$DIRECTORY/util/util.sh"
 
-NOCHECK=0
+nocheck=0
 
 for arg in "$@"; do
     shift
@@ -11,18 +11,24 @@ for arg in "$@"; do
         --yes)
             set -- "$@" "-y" 
             ;;
+        *)
+            set -- "$@" "$arg"
     esac
 done
 OPTIND=1
-while getopts "y" opt; do
+OPTERR=0
+while getopts ":y" opt; do
     case "$opt" in
         "y")
-            NOCHECK=1
+            nocheck=1
             ;;
-        "*")
-            show-error-option "paki update" "$opt"
+        *)
+            show-error-option "paki update" "-$OPTARG"
     esac
 done
+if [ "$OPTIND" -le "$#" ]; then
+    show-error-argument "paki update" "${!OPTIND}"
+fi
 
 if [ -z "$LINUX_SYSTEM" -o -z "$PACKAGER" ]; then
     echo "paki: could not find any suitable package manager in your PATH" 1>&2
@@ -31,21 +37,21 @@ fi
 
 case "$LINUX_SYSTEM" in
     debian)
-        if [ "$NOCHECK" -eq 1 ]; then
+        if [ "$nocheck" -eq 1 ]; then
             $SUDO_PACKAGER update && $SUDO_PACKAGER upgrade -y 
         else
             $SUDO_PACKAGER update && $SUDO_PACKAGER upgrade 
         fi
         ;;
     redhat)
-        if [ "$NOCHECK" -eq 1 ]; then
+        if [ "$nocheck" -eq 1 ]; then
             $SUDO_PACKAGER update -y
         else
             $SUDO_PACKAGER update
         fi
         ;;
     arch)
-        if [ "$NOCHECK" -eq 1 ]; then
+        if [ "$nocheck" -eq 1 ]; then
             $SUDO_PACKAGER -Syu --nocheck
         else
             $SUDO_PACKAGER -Syu
@@ -63,7 +69,7 @@ fi
 
 
 if [ ! -z "$FLATPAK_PACKAGER" ]; then
-    if [ "$NOCHECK" -eq 1 ]; then
+    if [ "$nocheck" -eq 1 ]; then
         $FLATPAK_PACKAGER update -y
     else
         $FLATPAK_PACKAGER update
